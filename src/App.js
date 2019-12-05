1a3e243847c74c46c534d6e2470e8d4fef83cfb6
Vincent Adeniji <adenijitosin112@gmail.com>
2019-12-04 19:53:23 -0500

    Styled and Installed React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => (
+  <Router>
   <div>
     <Navigation />
   </div>
+  </Router>
 );
 
 export default App;
